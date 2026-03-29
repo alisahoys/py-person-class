ab067df72d65e8c18b8c66ac9ae9ead4d11f4445
Alisa Hoys <hoys@protonmail.com>
2026-03-29 12:19:13 +0200

    Person task solution#2

--- app/main.py
+++ app/main.py
@@ -8,11 +8,8 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    people_instances = []
 
-    for person in people:
-        person_to_add = Person(person["name"], person["age"])
-        people_instances.append(person_to_add)
+    people_instances = [Person(person["name"], person["age"]) for person in people]
 
     for person in people:
         current_person = Person.people[person["name"]]
