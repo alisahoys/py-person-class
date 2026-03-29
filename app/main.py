9eadb3ab7bd5facc8a862919ca2ad5ebcc2c181e
Alisa Hoys <hoys@protonmail.com>
2026-03-29 12:23:14 +0200

    Person task solution#4

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-
+    Person.people.clear()
     people_instances = [Person(person["name"],
                                person["age"]) for person in people]
 
